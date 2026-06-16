eec7682601d60e97f24d849bd862fc4a06b7ee9e
B2B-Consulting <brianleehuddleston@gmail.com>
2026-06-15 21:08:04 -0500

    Avoid loser bracket rematches

--- components/PoolTournamentBracket.tsx
+++ components/PoolTournamentBracket.tsx
@@ -83,13 +83,13 @@ const thisWeekBracketSlots: BracketState = {
   "w3-2-a": { name: "Collin", score: "" },
   "w3-2-b": { name: "Roger", score: "" },
   "l2-1-a": { name: "Brendan", score: "" },
-  "l2-1-b": { name: "Hank", score: "" },
+  "l2-1-b": { name: "Jeremy", score: "" },
   "l2-2-a": { name: "Missy", score: "" },
   "l2-2-b": { name: "Joe", score: "" },
   "l2-3-a": { name: "Mack", score: "" },
-  "l2-3-b": { name: "Jeremy", score: "" },
+  "l2-3-b": { name: "Tolley", score: "" },
   "l2-4-a": { name: "Matt", score: "" },
-  "l2-4-b": { name: "Tolley", score: "" },
+  "l2-4-b": { name: "Hank", score: "" },
 };
 
 const bracketColumns: BracketColumn[] = [
